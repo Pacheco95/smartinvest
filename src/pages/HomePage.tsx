8b2cd609497a20fecb8266521956d2d0a92d19ae
Michael Pacheco <michael.dias@luizalabs.com>
2021-12-14 21:27:51 -0300

    fix: fix page bg color

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import { Card } from 'antd'
 
 export const HomePage = () => {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen">
       <div className="flex flex-col gap-8 p-2 md:p-4 m-auto md:w-3/4 lg:w-3/5 xl:w-1/2">
         <Card title="Conversor de taxas">
           <TaxConverter />
