b46e6c87f09c31528cf22d654439a58d9d8e84a3
Michael Pacheco <michael.dias@luizalabs.com>
2021-12-14 21:42:00 -0300

    style: improve home page style

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,10 +6,16 @@ export const HomePage = () => {
   return (
     <div className="flex h-screen">
       <div className="flex flex-col gap-8 p-2 md:p-4 m-auto md:w-3/4 lg:w-3/5 xl:w-1/2">
-        <Card title="Conversor de taxas">
+        <Card
+          title="Conversor de taxas"
+          className="!rounded border-8 shadow-lg"
+        >
           <TaxConverter />
         </Card>
-        <Card title="Simulador de investimento">
+        <Card
+          title="Simulador de investimentos"
+          className="!rounded border-8 shadow-lg"
+        >
           <Simulator />
         </Card>
       </div>
